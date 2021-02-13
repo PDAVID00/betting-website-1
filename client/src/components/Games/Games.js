668439859eebbdea36a8edd1683798a75d7f4710
Tiago-0liveira <tfgoliveira@gmail.com>
2021-02-13 00:03:22 +0000

    changed Game to GameCard

--- client/src/components/Games/Games.js
+++ client/src/components/Games/Games.js
@@ -9,47 +9,46 @@ import {
 	faBriefcase,
 } from "@fortawesome/free-solid-svg-icons";
 
-import Game from "../Game/Game";
+import GameCard from "../GameCard/GameCard";
 import "./Games.scss";
-
 
 const Games = () => {
 	return (
 		<div className="Games">
 			<h2 className="headerName">Games Home</h2>
 			<div className="display-divs">
-				<Game label="Roulette" imgSrc="Roulette">
+				<GameCard label="Roulette" imgSrc="Roulette">
 					<Link to="/Games/roulette">
 						<button>
 							<span>Play</span>
 							<FontAwesomeIcon icon={faCoins} />
 						</button>
 					</Link>
-				</Game>
-				<Game label="BlackJack" imgSrc="BlackJack">
+				</GameCard>
+				<GameCard label="BlackJack" imgSrc="BlackJack">
 					<Link to="/Games/BlackJack">
 						<button>
 							<span>Play</span>
 							<FontAwesomeIcon icon={faDice} />
 						</button>
 					</Link>
-				</Game>
-				<Game label="Crash" imgSrc="Crash">
+				</GameCard>
+				<GameCard label="Crash" imgSrc="Crash">
 					<Link to="/Games/Crash">
 						<button>
 							<span>Play</span>
 							<FontAwesomeIcon icon={faChartLine} />
 						</button>
 					</Link>
-				</Game>
-				<Game label="JackPot" imgSrc="JackPot">
+				</GameCard>
+				<GameCard label="JackPot" imgSrc="JackPot">
 					<Link to="/Games/JackPot">
 						<button>
 							<span>Play</span>
 							<FontAwesomeIcon icon={faBriefcase} />
 						</button>
 					</Link>
-				</Game>
+				</GameCard>
 			</div>
 		</div>
 	);
